3c65942f236bbbce64450dba528833cefd6762f7
Daniel-Cervera <daniel.cervera.2029@gmail.com>
2025-04-25 10:57:46 -0600

    add custom changes to ui views

--- mysite/lead/views.py
+++ mysite/lead/views.py
@@ -1,6 +1,7 @@
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import AddLeadForm
 from .models import Lead
@@ -16,10 +17,36 @@ def leads_list(request):
 
 @login_required
 def lead_detail(request, pk):
-    lead = Lead.objects.filter(created_by=request.user).get(pk=pk)
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
     return render(request, 'lead/leads_detail.html',{
         'lead' : lead
     })
+
+@login_required
+def leads_delete(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    lead.delete()
+
+    messages.success(request, 'The lead was deleted.')
+
+    return redirect('leads_list')
+
+@login_required
+def leads_edit (request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+        if form.is_valid():
+                    form.save()
+
+                    messages.success(request, 'The changes was saved.')
+                    return redirect('leads_list')
+        else:
+            form = AddLeadForm
+
+        return render(request, 'lead/leads_edit.html', {
+            'form' : form
+        })
 
 @login_required
 def add_lead(request):
@@ -29,6 +56,8 @@ def add_lead(request):
             lead = form.save(commit=False)
             lead.created_by = request.user
             lead.save()
+
+            messages.success(request, 'The lead was created.')
 
             return redirect('dashboard')
     else:
