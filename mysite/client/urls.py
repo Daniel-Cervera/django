56a54f28fac6665b4a6cd40fda6ab8d9add648c5
Daniel-Cervera <daniel.cervera.2029@gmail.com>
2025-04-29 05:27:11 -0600

    create a new template to view add_clients, and fix the request in def clients_list that makes dont render the clients

--- mysite/client/urls.py
+++ mysite/client/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = (
     path('', views.clients_list, name='clients_list'),
+    path('add-client/', views.add_clients, name='add_clients'),
 )
--- mysite/client/views.py
+++ mysite/client/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import AddClientForm
+from .models import Client
 
 # Create your views here.
 def clients_list(request):
-    return render(request, "client/clients_list.html")
+    clients = Client.objects.filter(created_by=request.user)
+
+    return render (request, 'client/clients_list.html', { 
+        'clients' : clients
+    })
+
+def add_clients(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        description = request.POST.get('description')
+
+        Client.objects.create(
+            name=name,
+            email=email,
+            description=description,
+
+            created_by=request.user
+        )
+        return redirect('clients_list')
+
+    return render(request, 'client/add_client.html')
